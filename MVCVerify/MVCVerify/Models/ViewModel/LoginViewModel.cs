f81ce830ea5620128022df35cb8ee78d497f5ab6
garycheng45 <garycheng45@hotmail.com>
2017-07-13 11:09:21 +0800

    建立LoginViewModel
    建立LoginViewModel

---
+++ MVCVerify/MVCVerify/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Accounting.Attribute;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCVerify.Models.ViewModel
+{
+	public class LoginViewModel
+	{
+		[Required]
+		[BanAccount]
+		[EmailAddress]
+		[Display(Name = "帳號")]
+		public string Account { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[StringLength(20, ErrorMessage = "{0}長度必須介於{1}到{2}之間", MinimumLength = 4)]
+		[Display(Name = "密碼")]
+		public string Password { get; set; }
+	}
+}

2e8764c44f97acaee9919e21e2f9af0ee6809b16
garycheng45 <garycheng45@hotmail.com>
2017-07-16 14:27:57 +0800

    登入功能
    加入登入頁面以及登入control

---
+++ MVCVerify/MVCVerify/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCVerify.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+			return View();
+			//if(ModelState.IsValid == false)
+			//{
+			//	return View();
+			//}
+			//return RedirectToAction("home", "index");
+        }
+		[HttpPost]
+		public ActionResult Index(Models.ViewModel.LoginViewModel model)
+		{
+			if(ModelState.IsValid == false)
+			{
+				return View();
+			}
+			return RedirectToAction("index", "home");
+		}
+    }
+}
